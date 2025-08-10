440176af3dbf7d1978aaf919785d9e188c06aefa
tolvanea <alpi.tolvanen@pm.me>
2025-08-11 01:06:57 +0300

    Add symbol resolution

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use regex::Regex;
 use compact_str::CompactString;
 use std::iter::Peekable;
 use slotmap::Key;
+use std::collections::HashMap;
 
+#[derive(Clone)]
 enum AstNode {
-    Definition(CompactString, DefaultKey),
+    Definition(CompactString, Vec<DefaultKey>, DefaultKey), // Symbol, symbol refs, function body
     Application(DefaultKey, DefaultKey),
     Symbol(CompactString),
 }
@@ -14,8 +16,9 @@ impl AstNode {
     fn print(&self, tree: &SlotMap<DefaultKey, AstNode>, indt: usize) -> String {
         let indent = " ".repeat(4*indt);
         match self {
-            AstNode::Definition(s, t) => format!(
-                "{indent}λ {s} (\n{}{indent})\n",
+            AstNode::Definition(s, ss, t) => format!(
+                "{indent}λ {s} [{}] (\n{}{indent})\n",
+                ss.len(),
                 tree.get(*t).unwrap().print(tree, indt + 1),
             ),
             AstNode::Application(t1, t2) => format!(
@@ -28,6 +31,7 @@ impl AstNode {
     }
 }
 
+#[derive(Clone)]
 struct Ast {
     head: DefaultKey,
     tree: SlotMap<DefaultKey, AstNode>,
@@ -35,49 +39,62 @@ struct Ast {
 
 impl Ast {
     fn new(source_code: &str) -> Ast {
-        let re = Regex::new(r"([()])|(λ\s?[^()λ\.]+\s?)\.|([^()λ\s\.]+)").unwrap();
+        let re = Regex::new(r"([()])|(λ\s?[^()λ\s\.]+)\s?\.|([^()λ\s\.]+)").unwrap();
         let mut tokens = re.captures_iter(&source_code).map(|c| c.extract::<1>().1[0]).peekable();
         let mut ast = Ast {head: DefaultKey::null(), tree: SlotMap::new()};
-        ast.head = ast.parse(&mut tokens, 0);
+        let mut symbols = HashMap::<CompactString, Vec<Vec<DefaultKey>>>::new();
+        ast.head = ast.parse(&mut tokens, &mut symbols, 0);
         ast
     }
 
     fn parse<'a>(
         &mut self,
         tokens: &mut Peekable<impl Iterator<Item = &'a str>>,
+        symbols: &mut HashMap<CompactString, Vec<Vec<DefaultKey>>>,
         indt: usize,
     ) -> DefaultKey {
         let token = tokens.next().expect("Ran out of tokens");
 
         println!("{}token: '{}'", " ".repeat(4*indt), token);
-
-        let parsed = match token.chars().next().unwrap() {
+        match token.chars().next().unwrap() {
             'λ' => {
-                let ast = AstNode::Definition(token[2..].into(), self.parse(tokens, indt+1));
+                let symbol: CompactString = token[2..].trim_start().into();
+                symbols.entry(symbol.clone())
+                    .and_modify(|vec| vec.push(Vec::new()))
+                    .or_insert(vec![Vec::new()]);
+                let sub_ast = self.parse(tokens, symbols, indt+1);
+                let references = symbols.get_mut(&symbol).unwrap().pop().unwrap(); // Scope visibility
+                let ast = AstNode::Definition(symbol, references, sub_ast);
                 println!("{}processed {}", " ".repeat(4*indt), token);
-                ast
+                self.tree.insert(ast)
             }
             '(' => {
-                let ast1 = self.parse(tokens, indt+1);
-                let ast3 = match *tokens.peek().unwrap() {
+                let ast1 = self.parse(tokens, symbols, indt+1);
+                let ast = match *tokens.peek().unwrap() {
                     ")" => {
-                        assert_eq!(tokens.next(), Some(")"));
-                        return ast1 // Only one expression: no need to wrap it
+                        ast1 // Only one expression: no need to wrap it
                     }
-                    _ => AstNode::Application(ast1, self.parse(tokens, indt+1)),
+                    _ => {
+                        let ast2 = AstNode::Application(ast1, self.parse(tokens, symbols, indt+1));
+                        println!("{}processed )", " ".repeat(4*indt));
+                        self.tree.insert(ast2)
+                    }
                 };
                 assert_eq!(tokens.next(), Some(")"));
-                println!("{}processed )", " ".repeat(4*indt));
-                ast3
+                ast
             }
-            ')' => panic!("Syntax error"),
+            ')' => panic!("Syntax error ')' right before:\n{}", tokens.collect::<String>()),
             _ => {
                 println!("{}was leaf )", " ".repeat(4*indt));
-                AstNode::Symbol(token.into())
+                let symbol: CompactString = token.into();
+                let node = self.tree.insert(AstNode::Symbol(symbol.clone()));
+                match symbols.get_mut(&symbol) {
+                    Some(vec) => vec.last_mut().unwrap().push(node),
+                    None => panic!("Unknown symbol '{symbol}'"),
+                }
+                node
             }
-
-        };
-        self.tree.insert(parsed)
+        }
     }
 
     fn print(&self) {
@@ -96,7 +113,7 @@ fn main() {
     let true_fn = "λa.(λb.a)";
     let false_fn = "λa.(λb.b)";
     let and_nf = format!("λx.(λy.((x y) {false_fn}))");
-    //let input = "(λ x . ( λ y . ( x y ) ) (λ zzz . ( zzz )))";
+    //let input = format!("(λ x . ( λ y . ( x y ) ) (λ z . ( z )))");
     let input = format!("(({and_nf} {true_fn}) {false_fn})");
     println!("{input}");
     let ast = Ast::new(&input);
